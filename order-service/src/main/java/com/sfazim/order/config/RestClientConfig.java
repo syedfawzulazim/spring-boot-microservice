879284255db9670f7785c70d378d225455142def
Syed Fawzul Azim <syed.fawzul.azim@gmail.com>
2024-12-24 00:07:29 +0100

    feat: add restClient config

---
+++ order-service/src/main/java/com/sfazim/order/config/RestClientConfig.java
@@ -0,0 +1,29 @@
+package com.sfazim.order.config;
+
+import com.sfazim.order.client.InventoryClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+@Configuration
+public class RestClientConfig {
+
+    @Value("${inventory.url}")
+    private String inventoryServiceUrl;
+
+    @Bean
+    public InventoryClient inventoryClient(){
+        RestClient restClient = RestClient.builder()
+                .baseUrl(inventoryServiceUrl)
+                .build();
+
+        RestClientAdapter restClientAdapter = RestClientAdapter.create(restClient);
+        HttpServiceProxyFactory httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
+
+        return httpServiceProxyFactory.createClient(InventoryClient.class);
+
+    }
+}
