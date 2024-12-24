61eccd8e40682c78dc15d0bb3c9113c7fdcb03de
Syed Fawzul Azim <syed.fawzul.azim@gmail.com>
2024-12-24 22:55:03 +0100

    feat: adding resilience4j & actuator for circuit breaking and monitoring metrics

--- order-service/src/main/java/com/sfazim/order/config/RestClientConfig.java
+++ order-service/src/main/java/com/sfazim/order/config/RestClientConfig.java
@@ -4,9 +4,13 @@ import com.sfazim.order.client.InventoryClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
@@ -18,12 +22,20 @@ public class RestClientConfig {
     public InventoryClient inventoryClient(){
         RestClient restClient = RestClient.builder()
                 .baseUrl(inventoryServiceUrl)
+                .requestFactory(getClientRequestFactory())
                 .build();
 
         RestClientAdapter restClientAdapter = RestClientAdapter.create(restClient);
-        HttpServiceProxyFactory httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
+        HttpServiceProxyFactory httpServiceProxyFactory =
+                HttpServiceProxyFactory.builderFor(restClientAdapter).build();
 
         return httpServiceProxyFactory.createClient(InventoryClient.class);
+    }
 
+    private ClientHttpRequestFactory getClientRequestFactory() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout((int) Duration.ofSeconds(3).toMillis());
+        factory.setReadTimeout((int) Duration.ofSeconds(3).toMillis());
+        return factory;
     }
 }
