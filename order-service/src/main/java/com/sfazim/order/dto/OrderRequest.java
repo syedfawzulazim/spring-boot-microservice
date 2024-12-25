8d46ff9bf00a356c65bd0800177ee702b750ff4a
Syed Fawzul Azim <syed.fawzul.azim@gmail.com>
2024-12-25 22:56:21 +0100

    feat: added kafka broker to order service

--- order-service/src/main/java/com/sfazim/order/dto/OrderRequest.java
+++ order-service/src/main/java/com/sfazim/order/dto/OrderRequest.java
@@ -7,4 +7,10 @@ public record OrderRequest(
         String orderNumber,
         String skuCode,
         BigDecimal price,
-        Integer quantity) { }
+        Integer quantity,
+        UserDetails userDetails
+    )
+{
+
+    public record UserDetails(String email, String firstName, String lastName){}
+}
--- order-service/src/main/java/com/sfazim/order/service/OrderService.java
+++ order-service/src/main/java/com/sfazim/order/service/OrderService.java
@@ -2,19 +2,24 @@ package com.sfazim.order.service;
 
 import com.sfazim.order.client.InventoryClient;
 import com.sfazim.order.dto.OrderRequest;
+import com.sfazim.order.event.OrderPlacedEvent;
 import com.sfazim.order.model.Order;
 import com.sfazim.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.kafka.core.KafkaTemplate;
 
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class OrderService {
 
     private final OrderRepository orderRepository;
     private final InventoryClient  inventoryClient;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public void placeOrder(OrderRequest orderRequest){
         var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
@@ -27,6 +32,20 @@ public class OrderService {
             order.setQuantity(orderRequest.quantity());
 
             orderRepository.save(order);
+
+            // send message to kafka topic
+            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent(
+                    order.getOrderNumber(),
+                    orderRequest.userDetails().email()
+            );
+
+            log.info("Start - Sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
+
+            kafkaTemplate.send("order-placed", orderPlacedEvent);
+
+            log.info("End - Finished OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
+
+
         } else {
             throw new RuntimeException("Product with SkuCode " + orderRequest.skuCode() + " is not in stock");
         }
