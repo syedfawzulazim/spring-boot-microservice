9e04dc9efa3c06d1c895bdb6782afa9b54f34ba2
Syed Fawzul Azim <syed.fawzul.azim@gmail.com>
2024-12-27 00:02:12 +0100

    feat: adding schema registry for kafka and .avsc file for event schema

--- order-service/src/main/java/com/sfazim/order/service/OrderService.java
+++ order-service/src/main/java/com/sfazim/order/service/OrderService.java
@@ -34,10 +34,11 @@ public class OrderService {
             orderRepository.save(order);
 
             // send message to kafka topic
-            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent(
-                    order.getOrderNumber(),
-                    orderRequest.userDetails().email()
-            );
+            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent();
+            orderPlacedEvent.setOrderNumber(order.getOrderNumber());
+            orderPlacedEvent.setEmail(orderRequest.userDetails().email());
+            orderPlacedEvent.setFirstName(orderRequest.userDetails().firstName());
+            orderPlacedEvent.setLastName(orderRequest.userDetails().lastName());
 
             log.info("Start - Sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
 
